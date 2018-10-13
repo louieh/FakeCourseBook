fb1148a6e0029672c1ec99f095b0e7907387ebe5
louieh <louiehan1015@gmail.com>
2018-10-13 02:24:06 -0500

    add search class now

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ collection = db.courses
 app = Flask(__name__)
 
 
+# TODO 修改课程结束时间
+# TODO 查询条件不消失
+
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     item_list = ["class_title", "class_number", "class_section", "class_instructor", "class_day", "class_start_time",
@@ -27,26 +30,33 @@ def hello_world():
             if request.form[each_item]:
                 item_dict[each_item] = request.form[each_item]
         fuzzyquery = 1 if "fuzzyquery" in request.form else 0
+        if 'search' in request.form:
+            if "class_section" in item_dict.keys():
+                if fuzzyquery:
+                    item_dict['class_section'] = re.compile(str(item_dict.get("class_section")), re.I)
+            if "class_number" in item_dict.keys():
+                if fuzzyquery:
+                    item_dict['class_number'] = re.compile(str(item_dict.get("class_number")), re.I)
+            if "class_title" in item_dict.keys():
+                if fuzzyquery:
+                    item_dict['class_title'] = re.compile(str(item_dict.get("class_title")), re.I)
+            if "class_instructor" in item_dict.keys():
+                if fuzzyquery:
+                    item_dict['class_instructor'] = re.compile(str(item_dict.get("class_instructor")), re.I)
+            if "class_location" in item_dict.keys():
+                if fuzzyquery:
+                    item_dict['class_location'] = re.compile(str(item_dict.get("class_location")), re.I)
 
-        if "class_section" in item_dict.keys():
-            if fuzzyquery:
-                item_dict['class_section'] = re.compile(str(item_dict.get("class_section")), re.I)
-        if "class_number" in item_dict.keys():
-            if fuzzyquery:
-                item_dict['class_number'] = re.compile(str(item_dict.get("class_number")), re.I)
-        if "class_title" in item_dict.keys():
-            if fuzzyquery:
-                item_dict['class_title'] = re.compile(str(item_dict.get("class_title")), re.I)
-        if "class_instructor" in item_dict.keys():
-            if fuzzyquery:
-                item_dict['class_instructor'] = re.compile(str(item_dict.get("class_instructor")), re.I)
-        if "class_location" in item_dict.keys():
-            if fuzzyquery:
-                item_dict['class_location'] = re.compile(str(item_dict.get("class_location")), re.I)
-
-        courses_list = list(collection.find(item_dict))
-
-        return render_template('search.html', data=courses_list)
+            courses_list = list(collection.find(item_dict))
+            return render_template('search.html', data=courses_list)
+        if 'nowclass' in request.form:
+            week_now = time.strftime("%A", time.localtime(time.time()))
+            time_now = datetime.datetime.now().strftime('%H:%M')
+            item_dict['class_day'] = re.compile(week_now, re.I)
+            item_dict['class_start_time'] = {"$lte": time_now}
+            item_dict['class_end_time'] = {"$gte": time_now}
+            courses_list = list(collection.find(item_dict))
+            return render_template('search.html', data=courses_list)
     return render_template('search.html')
 
 
