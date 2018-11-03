fd8d43c05948237ae70af5d651735a3c04714956
louieh <louiehan1015@gmail.com>
2018-11-02 23:11:00 -0500

    add config to transfer the time of data update

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ collection = db.courses19spring
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
+app.config.from_object('config')
+
+print(app.config['INSERT_DATA_TIME'])
 
 
 def getRateId(name):
---
+++ config.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+INSERT_DATA_TIME = '123'
