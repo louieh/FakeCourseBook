4dad7ba7de2593a5c892cb0a9852519a4e023db2
louieh <louiehan1015@gmail.com>
2019-04-03 00:43:39 -0500

    Sort the dictionary to avoid duplication due to different key sequences.

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ import requests
 import logging
 import os
 import redis
+from collections import OrderedDict
 
 client = MongoClient("localhost", 27017)
 db = client.Coursebook
@@ -171,19 +172,14 @@ def graph_pro(professor=None, coursesection=None):
         elif "graph/course" in request.url:
             cou_set = set()
             cou_dict_list = list(db.CourseForGraph.find({}, {"class_title": 1, "class_section": 1, "_id": 0}))
-            cou_dict_list_num = len(cou_dict_list)
             for eachcou_dict in cou_dict_list:
                 eachcou_dict["class_section"] = eachcou_dict.get("class_section").split(".")[0]
-                cou_set.add(json.dumps(eachcou_dict))
-            cou_set_num = len(cou_set)
+                cou_set.add(json.dumps(OrderedDict(sorted(eachcou_dict.items(), key=lambda t: t[0])))) # Sort the dict to avoid duplication due to different key sequences.
             cou_dict_list_temp = sorted(list(cou_set))
-            cou_dict_list_temp_num = len(cou_dict_list_temp)
             cou_dict_list_fin = []
             for eachcou_dict in cou_dict_list_temp:
                 cou_dict_list_fin.append(json.loads(eachcou_dict))
-            cou_dict_list_fin_num = len(cou_dict_list_fin)
-            return render_template("graph.html", cou_dict_list=cou_dict_list_fin, cou_dict_list_num=cou_dict_list_num,
-                                   cou_set_num=cou_set_num,cou_dict_list_temp_num=cou_dict_list_temp_num,cou_dict_list_fin_num=cou_dict_list_fin_num)
+            return render_template("graph.html", cou_dict_list=cou_dict_list_fin)
 
     if professor:
         if not list(db.CourseForGraph.find({"class_instructor": professor})):
