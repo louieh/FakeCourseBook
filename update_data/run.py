1d56a9df1f34f0ed9fa29d3224cc1c51af76b57d
louieh <louiehan1015@gmail.com>
2019-11-05 14:13:17 -0600

    add apscheduler

--- update_data/run.py
+++ update_data/run.py
@@ -3,6 +3,21 @@
 
 import spider
 from log import logger
+from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
-spider = spider.Spider(update_method=1)
-spider.update_data()
+
+def main():
+    spider = spider.Spider(update_method=1)
+    spider.update_data()
+
+
+executors = {
+    'default': ThreadPoolExecutor(1),
+    'processpool': ProcessPoolExecutor(1)
+}
+timer = BlockingScheduler(executors=executors)
+
+if __name__ == "__main__":
+    timer.add_job(main, 'interval', hours=6)
+    timer.start()
