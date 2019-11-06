282324662056c3005824f67bb158ecf21a14395d
louieh <louiehan1015@gmail.com>
2019-11-06 02:18:42 -0600

    int

--- update_data/setting.py
+++ update_data/setting.py
@@ -49,4 +49,4 @@ TIMENOW = lambda: (datetime.datetime.utcnow() - datetime.timedelta(hours=TIMEDEL
 TIMENOW_UTC = lambda: datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M")
 
 # update_interval
-UPDATE_INTERVAL = float(os.getenv('UPDATE_INTERVAL', 360))
+UPDATE_INTERVAL = int(os.getenv('UPDATE_INTERVAL', 360))
