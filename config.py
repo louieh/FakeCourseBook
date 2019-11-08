709fc6a046ade3420560cf61005b1f76e3e31629
louieh <louiehan1015@gmail.com>
2019-11-08 16:06:02 -0600

    change the config of app

--- config.py
+++ config.py
@@ -11,14 +11,15 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MONGO_HOST = os.getenv('MONGO_HOST', 'mongo')
+    MONGO_HOST = os.getenv('MONGO_HOST', 'localhost')
     MONGO_PORT = int(os.getenv('MONGO_PORT', 27017))
     COLLECTION_NAME_FOR_SEARCH = 'CourseForSearch'
     COLLECTION_NAME_FOR_GRAPH = 'CourseForGraph'
-    REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
+    REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
     REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
-    REDIS_URL = os.getenv('REDIS_URL', 'redis://redis:6379')
+    REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
     REDIS_UPDATE_TIME_KEY = 'data_update_time'
+    REDIS_UPDATE_NEXT_TIME_KEY = 'data_update_next_time'
 
     @staticmethod
     def init_app(app):
@@ -30,7 +31,20 @@ class TestingConfig(Config):
 
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = False
+    MONGO_HOST = os.getenv('MONGO_HOST', 'mongo')
+    MONGO_PORT = int(os.getenv('MONGO_PORT', 27017))
+    COLLECTION_NAME_FOR_SEARCH = 'CourseForSearch'
+    COLLECTION_NAME_FOR_GRAPH = 'CourseForGraph'
+    REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
+    REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+    REDIS_URL = os.getenv('REDIS_URL', 'redis://redis:6379')
+    REDIS_UPDATE_TIME_KEY = 'data_update_time'
+    REDIS_UPDATE_NEXT_TIME_KEY = 'data_update_next_time'
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 config = {
