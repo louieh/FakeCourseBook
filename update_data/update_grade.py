242153e32f5808747f3bf45e0f9f7d68142cee0e
louieh <louiehan1015@gmail.com>
2020-02-07 23:23:57 -0600

    add data of summer2019 and fall2019

--- update_data/update_grade.py
+++ update_data/update_grade.py
@@ -2,11 +2,15 @@ import requests
 import json
 from pymongo import MongoClient
 
-urls = ["https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Fall%202017/fall2017.json",
-        "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Fall%202018/fall2018.json",
-        "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Spring%202018/spring2018.json",
-        "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Spring%202019/spring2019.json",
-        "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Summer%202018/summer2018.json"]
+urls = [
+    # "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Fall%202017/fall2017.json",
+    # "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Fall%202018/fall2018.json",
+    # "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Spring%202018/spring2018.json",
+    # "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Spring%202019/spring2019.json",
+    # "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Summer%202018/summer2018.json",
+    "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Fall%202019/Fall2019.json",
+    "https://raw.githubusercontent.com/bharatari/utd-grades/master/data/Summer%202019/Summer2019.json"
+]
 
 client = MongoClient("localhost", 27017)
 db = client.Coursebook
