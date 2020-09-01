fbe2338661bdb5d619198d357a29aa796d040435
louieh <louiehan1015@gmail.com>
2020-09-01 00:00:07 -0500

    update parser

--- update_data/parser.py
+++ update_data/parser.py
@@ -89,7 +89,7 @@ class Parser(object):
                     # TODO each_course_dict['_id'] = eachclass_section_number[-1]
 
                     # class_title
-                    class_title = each_course_selector.xpath('''//td[3]//text()''')
+                    class_title = each_course_selector.xpath('''//td[4]//text()''')
                     if class_title:
                         each_course_dict['class_title'] = class_title[0].replace("(3 Semester Credit Hours)",
                                                                                  "").replace(
@@ -98,20 +98,20 @@ class Parser(object):
 
                     # class_instructor
                     class_instructor = each_course_selector.xpath(
-                        '''//td[4]//text()''')  # ['Don Vogel', '\n, ', 'Stephen Perkins', '\n']
+                        '''//td[5]//text()''')  # ['Don Vogel', '\n, ', 'Stephen Perkins', '\n']
                     if class_instructor:
                         class_instructor = [x for x in class_instructor if '\n' not in x]
                         each_course_dict['class_instructor'] = class_instructor
 
                     # class_day class_time class_start_time class_end_time class_location
                     class_day = each_course_selector.xpath(
-                        '''//td[5]/div/span[@class="clstbl__resultrow__day"]//text()''')
+                        '''//td[6]/div/span[@class="clstbl__resultrow__day"]//text()''')
                     if class_day:
                         each_course_dict['class_day'] = class_day[0]
 
                         class_time_final = ''
                         class_time = each_course_selector.xpath(
-                            '''//td[5]/div/span[@class="clstbl__resultrow__time"]//text()''')  # ['1:00pm - 3:45pm']
+                            '''//td[6]/div/span[@class="clstbl__resultrow__time"]//text()''')  # ['1:00pm - 3:45pm']
                         if class_time:
                             class_time = class_time[0]
                         for each_time in class_time.split("-"):
@@ -129,20 +129,20 @@ class Parser(object):
                         each_course_dict['class_start_time'] = each_course_dict['class_time'].split("-")[0]
                         each_course_dict['class_end_time'] = each_course_dict['class_time'].split("-")[-1]
                         class_location = each_course_selector.xpath(
-                            '''//td[5]/div/div[@class="clstbl__resultrow__location"]//text()''')
+                            '''//td[6]/div/div[@class="clstbl__resultrow__location"]//text()''')
                         if class_location:
                             each_course_dict['class_location'] = class_location[0]
                     else:
-                        class_location = each_course_selector.xpath('''//td[5]//text()''')
+                        class_location = each_course_selector.xpath('''//td[6]//text()''')
                         if class_location:
                             each_course_dict['class_location'] = class_location[0]
 
                     # class_isFull
-                    class_ifFull = each_course_selector.xpath('''//td[6]/div/@title''')
+                    class_ifFull = each_course_selector.xpath('''//td[7]/div/@title''')
                     if class_ifFull:
                         each_course_dict['class_isFull'] = class_ifFull[0]
                     else:
-                        class_ifFull = each_course_selector.xpath('''//td[6]/div/@style''')
+                        class_ifFull = each_course_selector.xpath('''//td[7]/div/@style''')
                         if class_ifFull:
                             class_ifFull = round(abs(int(class_ifFull[0].split(" ")[-2].replace("px", ""))) / 320 * 100,
                                                  2)
