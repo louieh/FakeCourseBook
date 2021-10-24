9949fa46beec30a3529fa6fb57424a23adfb99d8
louieh <louiehan1015@gmail.com>
2021-10-24 17:47:38 +0800

    change semester

--- config.py
+++ config.py
@@ -10,10 +10,9 @@ class Config(object):
     CUSTOM_SEARCH_KEY = os.environ.get('CUSTOM_SEARCH_KEY')
     CUSTOM_SEARCH_UTD_ID = os.environ.get('CUSTOM_SEARCH_UTD_ID')
     CUSTOM_SEARCH_FAKECOURSEBOOK_ID = os.environ.get('CUSTOM_SEARCH_FAKECOURSEBOOK_ID')
-    TERM_LIST = ['20F', '20U', '20S', '19F', '19U', '19S', '18F', '18U', '18S', '17F', '17U', '17S', '16F', '16U',
-                 '16S', '15F', '15U',
-                 '15S', '14F', '14U', '14S', '13F', '13U', '13S', '12F', '12U', '12S', '11F', '11U', '11S', '10F',
-                 '10U', '10S']
+    TERM_LIST = ['22S', '21F', '21U', '21S', '20F', '20U', '20S', '19F', '19U', '19S', '18F', '18U', '18S', '17F',
+                 '17U', '17S', '16F', '16U', '16S', '15F', '15U', '15S', '14F', '14U', '14S', '13F', '13U', '13S',
+                 '12F', '12U', '12S', '11F', '11U', '11S', '10F', '10U', '10S']
     TIMEDELTA = 5  # summer time
     # TIMEDELTA = 6  # winter time
 
